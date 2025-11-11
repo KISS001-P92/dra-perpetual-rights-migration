bc9a4fc9a9d09f02f6c7b84844e3fad522591eba
KISS001 <judit.kiss.p92@sonymusic.com>
2025-11-11 09:00:16 +0100

    CAS-15082

--- src/main/resources/db/changelog/v_dra_perpetual_rights_migration_input.sql
+++ src/main/resources/db/changelog/v_dra_perpetual_rights_migration_input.sql
@@ -24,15 +24,15 @@ select dprus.id  id,
        dprus.mod_stamp,
        dprus.mod_user
 from dra_perpetual_rights_migration_input_us dprus
-         left join document_sony_artist dsa on dprus."Contract"::text = dsa.contract_number::text and dsa.status_flag ='A'
+left join document_sony_artist dsa on dprus."Contract"::text = dsa.contract_number::text and dsa.status_flag ='A'
 left join v_document vd on dsa.document_id = vd.document_id and dprus."Doc Type" = vd.document_type_name
-    left join contract c on dsa.document_id =c.document_id  and c.status_flag ='A'
-    left join node_instance ni on c.contract_id = ni.contract_id and path like '%Digital Rights%' and ni.status_flag ='A' and ni.period_id is not null and ni.adm_recording_id is  not null and ni.name=dprus."Recording"
-    left join digital_rights_contract_us drcu_ni on ni.node_instance_id = drcu_ni.node_instance_id  and drcu_ni.status_flag='A' and drcu_ni.dra_rights_hierarchy_id = 1
-    left join node_instance ni_parent on ni.parent_id = ni_parent.node_instance_id  and ni_parent.path like '%Digital Rights%' and ni_parent.status_flag ='A'
-    left join digital_rights_contract_us drcu_ni_parent on ni_parent.node_instance_id = drcu_ni_parent.node_instance_id and drcu_ni_parent.status_flag='A'and drcu_ni_parent.dra_rights_hierarchy_id = 1
-    left join node_instance ni_grand_parent on ni_parent.parent_id = ni_grand_parent.node_instance_id  and ni_grand_parent.path like '%Digital Rights%' and ni_grand_parent.status_flag ='A'
-    left join digital_rights_contract_us drcu_ni_grand_parent on ni_grand_parent.node_instance_id = drcu_ni_grand_parent.node_instance_id and drcu_ni_grand_parent.status_flag='A'and drcu_ni_grand_parent.dra_rights_hierarchy_id = 1
+left join contract c on dsa.document_id =c.document_id  and c.status_flag ='A'
+left join node_instance ni on c.contract_id = ni.contract_id and path like '%Digital Rights%' and ni.status_flag ='A' and ni.period_id is not null and ni.adm_recording_id is  not null and ni.name=dprus."Recording"
+left join digital_rights_contract_us drcu_ni on ni.node_instance_id = drcu_ni.node_instance_id  and drcu_ni.status_flag='A' and drcu_ni.dra_rights_hierarchy_id = 1
+left join node_instance ni_parent on ni.parent_id = ni_parent.node_instance_id  and ni_parent.path like '%Digital Rights%' and ni_parent.status_flag ='A'
+left join digital_rights_contract_us drcu_ni_parent on ni_parent.node_instance_id = drcu_ni_parent.node_instance_id and drcu_ni_parent.status_flag='A'and drcu_ni_parent.dra_rights_hierarchy_id = 1
+left join node_instance ni_grand_parent on ni_parent.parent_id = ni_grand_parent.node_instance_id  and ni_grand_parent.path like '%Digital Rights%' and ni_grand_parent.status_flag ='A'
+left join digital_rights_contract_us drcu_ni_grand_parent on ni_grand_parent.node_instance_id = drcu_ni_grand_parent.node_instance_id and drcu_ni_grand_parent.status_flag='A'and drcu_ni_grand_parent.dra_rights_hierarchy_id = 1
 union
 select dprus.id as id,
        dprus.country_id,
@@ -59,12 +59,12 @@ select dprus.id as id,
        dprus.mod_stamp,
        dprus.mod_user
 from dra_perpetual_rights_migration_input_eu dprus
-         left join document_european_ba dsa on dprus."Contract" = dsa.contract_number and dsa.status_flag ='A'
-         left join v_document vd on dsa.document_id = vd.document_id and dprus."Doc Type" = vd.document_type_name
-         left join contract c on dsa.document_id =c.document_id  and c.status_flag ='A'
-         left join node_instance ni on c.contract_id = ni.contract_id and path like '%Digital Rights%' and ni.status_flag ='A' and ni.period_id is not null and ni.adm_recording_id is  not null and ni.name=dprus."Recording"
-         left join digital_rights_contract_eu drcu_ni on ni.node_instance_id = drcu_ni.node_instance_id  and drcu_ni.status_flag='A' and drcu_ni.dra_rights_hierarchy_id = 1
-         left join node_instance ni_parent on ni.parent_id = ni_parent.node_instance_id  and ni_parent.path like '%Digital Rights%' and ni_parent.status_flag ='A'
-         left join digital_rights_contract_eu drcu_ni_parent on ni_parent.node_instance_id = drcu_ni_parent.node_instance_id and drcu_ni_parent.status_flag='A'and drcu_ni_parent.dra_rights_hierarchy_id = 1
-         left join node_instance ni_grand_parent on ni_parent.parent_id = ni_grand_parent.node_instance_id  and ni_grand_parent.path like '%Digital Rights%' and ni_grand_parent.status_flag ='A'
-         left join digital_rights_contract_eu drcu_ni_grand_parent on ni_grand_parent.node_instance_id = drcu_ni_grand_parent.node_instance_id and drcu_ni_grand_parent.status_flag='A'and drcu_ni_grand_parent.dra_rights_hierarchy_id = 1
+left join document_european_ba dsa on dprus."Contract" = dsa.contract_number and dsa.status_flag ='A'
+left join v_document vd on dsa.document_id = vd.document_id and dprus."Doc Type" = vd.document_type_name
+left join contract c on dsa.document_id =c.document_id  and c.status_flag ='A'
+left join node_instance ni on c.contract_id = ni.contract_id and path like '%Digital Rights%' and ni.status_flag ='A' and ni.period_id is not null and ni.adm_recording_id is  not null and ni.name=dprus."Recording"
+left join digital_rights_contract_eu drcu_ni on ni.node_instance_id = drcu_ni.node_instance_id  and drcu_ni.status_flag='A' and drcu_ni.dra_rights_hierarchy_id = 1
+left join node_instance ni_parent on ni.parent_id = ni_parent.node_instance_id  and ni_parent.path like '%Digital Rights%' and ni_parent.status_flag ='A'
+left join digital_rights_contract_eu drcu_ni_parent on ni_parent.node_instance_id = drcu_ni_parent.node_instance_id and drcu_ni_parent.status_flag='A'and drcu_ni_parent.dra_rights_hierarchy_id = 1
+left join node_instance ni_grand_parent on ni_parent.parent_id = ni_grand_parent.node_instance_id  and ni_grand_parent.path like '%Digital Rights%' and ni_grand_parent.status_flag ='A'
+left join digital_rights_contract_eu drcu_ni_grand_parent on ni_grand_parent.node_instance_id = drcu_ni_grand_parent.node_instance_id and drcu_ni_grand_parent.status_flag='A'and drcu_ni_grand_parent.dra_rights_hierarchy_id = 1
